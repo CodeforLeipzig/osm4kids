6748b1d2135d0bc5610c21890422304177e9da9b
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2016-06-22 21:35:03 +0200

    added mocha to package dependencies

--- test/api.js
+++ test/api.js
@@ -1,8 +1,17 @@
 var assert = require('assert'),
-    http = require('http');
+var http = require('http');
 
-describe('/schools', function () {
-  it('should return 200', function (done) {
+describe('HTTP status code after GET api/playgrounds/', function () {
+  it('should be 200', function (done) {
+    http.get('http://localhost:8000/api/playgrounds', function (res) {
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+});
+
+describe('HTTP status code after GET api/schools/', function () {
+  it('should be 200', function (done) {
     http.get('http://localhost:8000/api/schools', function (res) {
       assert.equal(200, res.statusCode);
       done();
@@ -10,8 +19,8 @@ describe('/schools', function () {
   });
 });
 
-describe('/doctors', function () {
-  it('should return 200', function (done) {
+describe('HTTP status code after GET api/doctors/', function () {
+  it('should be 200', function (done) {
     http.get('http://localhost:8000/api/doctors', function (res) {
       assert.equal(200, res.statusCode);
       done();
