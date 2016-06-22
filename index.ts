d68aea89ceada78626ba805132fef5a0c3975a0f
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2016-06-23 00:34:21 +0200

    added PORT environment variable for heroku deployment

--- index.ts
+++ index.ts
@@ -6,40 +6,42 @@ import body_parser = require('body-parser');
 import { OverpassJob } from './cron';
 import { pathSep } from './helpers';
 
-var resource_dir : string = 'resources';
-var query_dir : string = 'queries';
-var port_num : number = 8000;
-var app = express();
+let resource_dir : string = 'resources';
+let query_dir : string = 'queries';
+let local_port : number = 8000;
+let app = express();
 
 app.use(body_parser.urlencoded({extended: true}));
 app.use(body_parser.json());
+app.set('port', (process.env.PORT || local_port));
 
 /* Routes Definition */
 app.use(express.static('public'));
 app.use('/api/playgrounds', function (req, res, next) {
-    var complete_path : string = resource_dir + pathSep() + 'playgrounds.geojson';
+    let complete_path : string = resource_dir + pathSep() + 'playgrounds.geojson';
     fs.readFile(complete_path, 'utf8', function(err, data) {
         res.json(JSON.parse(data));
     });
 });
 
 app.use('/api/doctors', function (req, res, next) {
-    var complete_path : string = resource_dir + pathSep() + 'doctors.geojson';
+    let complete_path : string = resource_dir + pathSep() + 'doctors.geojson';
     fs.readFile(complete_path, 'utf8', function(err, data) {
         res.json(JSON.parse(data));
     });
 });
 
 app.use('/api/schools', function (req, res, next) {
-    var complete_path : string = resource_dir + pathSep() + 'schools.geojson';
+    let complete_path : string = resource_dir + pathSep() + 'schools.geojson';
     fs.readFile(complete_path, 'utf8', function(err, data) {
         res.json(JSON.parse(data));
     });
 });
 
 /* Start Server */
-app.listen(port_num);
-console.log('Magic happens on port ' + port_num + ".");
+app.listen(app.get('port'), function() {
+  console.log('Magic happens on port', app.get('port'));
+});
 
 /* Start Cronjob */
-let cronjob = new OverpassJob('* * */6 * * *', query_dir, resource_dir);
+let cronjob = new OverpassJob('*/10 * * * * *', query_dir, resource_dir);
