8151006b20e53a563f1e2b9093eaee4883a03039
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2016-06-22 20:03:05 +0200

    read resource files asynchronously

--- index.ts
+++ index.ts
@@ -1,33 +1,45 @@
 ﻿/// <reference path="typings/main.d.ts" />
 
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var body_parser = require('body-parser');
-import _ = require('lodash');
+import fs = require('fs');
+import express = require('express');
+import body_parser = require('body-parser');
 import { OverpassJob } from './cron';
-var port_num = 8000;
+import { pathSep } from './helpers';
+
+var resource_dir : string = 'resources';
+var query_dir : string = 'queries';
+var port_num : number = 8000;
+var app = express();
 
 app.use(body_parser.urlencoded({extended: true}));
 app.use(body_parser.json());
 
-/* Routes */
+/* Routes Definition */
 app.use(express.static('public'));
 app.use('/api/playgrounds', function (req, res, next) {
-    res.json( JSON.parse(fs.readFileSync('./resources/playgrounds.geojson')) );
+    var complete_path : string = resource_dir + pathSep() + 'playgrounds.geojson';
+    fs.readFile(complete_path, 'utf8', function(err, data) {
+        res.json(JSON.parse(data));
+    });
 });
 
 app.use('/api/doctors', function (req, res, next) {
-    res.json( JSON.parse(fs.readFileSync('./resources/doctors.geojson')) );
+    var complete_path : string = resource_dir + pathSep() + 'doctors.geojson';
+    fs.readFile(complete_path, 'utf8', function(err, data) {
+        res.json(JSON.parse(data));
+    });
 });
 
 app.use('/api/schools', function (req, res, next) {
-    res.json( JSON.parse(fs.readFileSync('./resources/schools.geojson')) );
+    var complete_path : string = resource_dir + pathSep() + 'schools.geojson';
+    fs.readFile(complete_path, 'utf8', function(err, data) {
+        res.json(JSON.parse(data));
+    });
 });
 
 /* Start Server */
 app.listen(port_num);
-console.log('Magic happens on port ' + port_num);
+console.log('Magic happens on port ' + port_num + ".");
 
 /* Start Cronjob */
-let cronjob = new OverpassJob('* * */6 * * *', 'queries', 'resources');
+let cronjob = new OverpassJob('* * */6 * * *', query_dir, resource_dir);
