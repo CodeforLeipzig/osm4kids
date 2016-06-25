7547e2dcd35834bcdf5b10eca4792f8b20097ac5
Hans Christian Rudolph <hanschristianrudolph@gmail.com>
2016-06-25 11:40:56 +0200

    bumped typings to v1.3.0

--- typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="browser\ambient\body-parser\body-parser.d.ts" />
-/// <reference path="browser\ambient\express\express.d.ts" />
-/// <reference path="browser\ambient\lodash\lodash.d.ts" />
-/// <reference path="browser\ambient\mime\mime.d.ts" />
-/// <reference path="browser\ambient\node\node.d.ts" />
-/// <reference path="browser\ambient\serve-static\serve-static.d.ts" />
